e626f74cb1be3528d8dcc107c3d8e3d7b08b16b3
Victor Godoy <142838760+victorrgodoy@users.noreply.github.com>
2024-10-02 22:41:48 -0300

    Add files via upload

---
+++ java-programming-2/Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class PositiveNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // test your method here
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(10);
+        arrayList.add(-10);
+        System.out.println(positive(arrayList));
+
+    }
+
+    public static List<Integer> positive(List<Integer> numbers){
+        ArrayList<Integer> arrayList =  numbers.stream()
+                                        .filter(n -> n > 0)
+                                        .collect(Collectors.toCollection(ArrayList::new));
+
+        return arrayList;
+    }
+
+}
