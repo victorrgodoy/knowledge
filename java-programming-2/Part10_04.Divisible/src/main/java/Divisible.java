1ad771d9bd9451fb4cf05f7b9ebb8eb2f9a51a32
Victor Godoy <142838760+victorrgodoy@users.noreply.github.com>
2024-10-13 15:18:50 -0300

    Update Divisible.java

--- java-programming-2/Part10_04.Divisible/src/main/java/Divisible.java
+++ java-programming-2/Part10_04.Divisible/src/main/java/Divisible.java
@@ -6,23 +6,23 @@ import java.util.stream.Collectors;
 public class Divisible {
 
     public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>(); // array list of numbers
         numbers.add(3);
         numbers.add(2);
         numbers.add(-17);
         numbers.add(-5);
         numbers.add(7);
 
-        ArrayList<Integer> divisible = divisible(numbers);
+        ArrayList<Integer> divisible = divisible(numbers); 
 
         divisible.stream()
-                .forEach(num -> System.out.println(num));
+                .forEach(num -> System.out.println(num)); // for each in stream
     }
 
-    public static ArrayList<Integer> divisible(ArrayList<Integer> numbers) {
-        return numbers.stream()
-                .filter(n -> n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
-                .collect(Collectors.toCollection(ArrayList::new));
+    public static ArrayList<Integer> divisible(ArrayList<Integer> numbers) { // // method to get the divisor numbers of 2,3 and 5 and return new list with numbers
+        return numbers.stream() 
+                .filter(n -> n % 2 == 0 || n % 3 == 0 || n % 5 == 0) // filter the numbers 
+                .collect(Collectors.toCollection(ArrayList::new)); // collect numbers for put in the new array list 
 
     }
 
